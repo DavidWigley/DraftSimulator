e66bc638fc99b8ca7c71fd79f73e204f5ada5721
David <dwigley92@gmail.com>
2014-05-21 16:23:21 -0400

    More input logic.

--- DraftSimulator/src/Draft/Logic.java
+++ DraftSimulator/src/Draft/Logic.java
@@ -12,39 +12,103 @@ public class Logic {
 	
 	public int YearInput() {
 		int year = -1;
-		boolean done;
-		
-		
-		return year;
-	}
-	
-	public int MonthInput() {
-		int num = -1;
 		boolean done = false;
 		while (!done) {
 			String choice = JOptionPane.showInputDialog(null, "Enter a month. Please use numbers IE: Jan = 1, Feb = 2, etc");
 			try	{
-			num = Integer.parseInt(choice);
+				year = Integer.parseInt(choice);
 			} catch (NumberFormatException nfe) {
-				num = -1;
+				year = -1;
 			}
-			if (num == -1 || num==0 || num > 12) {
+			if (year <= 1900) {
 				JOptionPane.showMessageDialog(null, "You did not enter a valid number for the month");
+			}else {
+				done = true;
 			}
 		}
-		month = num;
-		return num;
+		this.year = year;
+		return year;
+	}
+	
+	public int MonthInput() {
+		int month = -1;
+		boolean done = false;
+		while (!done) {
+			String choice = JOptionPane.showInputDialog(null, "Enter a month. Please use numbers IE: Jan = 1, Feb = 2, etc");
+			try	{
+				month = Integer.parseInt(choice);
+			} catch (NumberFormatException nfe) {
+				month = -1;
+			}
+			if (month == -1 || month==0 || month > 12) {
+				JOptionPane.showMessageDialog(null, "You did not enter a valid number for the month");
+			} else {
+				done = true;
+			}
+		}
+		this.month = month;
+		return month;
 		
 	}
 	
 	public int DayInput() {
 		boolean done = false;
-		int num = -1;
+		int day = -1;
 		while (!done) {
 			String choice = JOptionPane.showInputDialog(null, "Enter the day");
+			try	{
+				day = Integer.parseInt(choice);
+			} catch (NumberFormatException nfe) {
+				day = -1;
+			}
+			
 		}
 		
-		return num;
+		return day;
+	}
+	
+	public boolean IsLeapYear() {
+		boolean leapYear = false;;
+		if (year % 4 == 0) {
+			leapYear = true;
+			if (year % 100 == 0) {
+				if (year % 400 == 0) {
+					leapYear = true;
+				} else {
+					leapYear = false;
+				}
+			}
+		}
+		return leapYear;
+	}
+	
+	public boolean ValidDay(int day, int month) {
+		boolean valid = false;
+		//months with 31 days
+		if (month == 1 || month == 3 || month == 5 || month == 7 || month == 8 || month == 10 || month == 12) {
+			if (day <= 31) {
+				valid = true;
+			}
+		}
+		//months with 30 days
+		else if (month == 4 || month == 6 || month == 9 || month == 11) {
+			if (day <= 30) {
+				valid = true;
+			}
+		}
+		//feb
+		else {
+			if (IsLeapYear()) {
+				if (day <= 29) {
+					valid = true;
+				}
+			}else {
+				if (day <= 28) {
+					valid = true;
+				}
+			}
+		}
+		return valid;
 	}
 
 }
